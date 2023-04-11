2615a3f60a0d90f81706834b7c6d372da9111487
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-11 18:49:40 +0000

    Updated the script for adding to quotes file

--- add_quotes.py
+++ add_quotes.py
@@ -1,24 +1,18 @@
 import csv 
 
 """ Python program to demonstrate to add more quotes to random-quotes. """
-# field names 
-fields = ['quote', 'author'] 
-    
-# A user can add rows of quotes and their authors to this list
-rows = [ ['A random quote', 'Author Name'], 
-         ['Another random quote', 'Author Name'] 
-        ] 
 
-# name of csv file 
-filename = "random-quotes.csv"
-    
-# writing to csv file 
-with open(filename, 'w') as csvfile: 
-    # creating a csv writer object 
-    csvwriter = csv.writer(csvfile) 
-        
-    # writing the fields 
-    csvwriter.writerow(fields) 
-        
-    # writing the data rows 
-    csvwriter.writerows(rows)
+def add_new_quote(filepath, quote, author):
+    # Create a dictionary for the new quote
+    new_quote = {"quote": quote, "author": author}
+
+    # Append(write) the new quote to the CSV file
+    with open(filepath, 'a', newline='') as csvfile:
+        fieldnames = ["quote", "author"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(new_quote)
+
+# Prompt the user to add a new quote
+quote = input("Enter a new quote: ")
+author = input("Enter the author of the quote: ")
+add_new_quote('random-quotes.csv', quote, author)
